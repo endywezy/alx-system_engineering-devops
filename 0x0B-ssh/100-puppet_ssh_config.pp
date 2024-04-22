a296b79bd3c9d4791bd2c5823d945109fb5a38ce
endywezy <endywezy@gmail.com>
2024-04-21 20:43:39 -0700

    SSH Server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,37 +1,16 @@
-#!/usr/bin/env bashi
-# Define SSH client configuration class
-class ssh_client_config {
+# Seting up my client config file
+include stdlib
 
-  # Ensure SSH client configuration directory exists
-  file { '/home/ubuntu/.ssh':
-    ensure  => 'directory',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    mode    => '0700',
-    require => File['/home/ubuntu'],
-  }
-
-  # Ensure SSH client configuration file exists
-  file { '/home/ubuntu/.ssh/config':
-    ensure  => 'file',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    mode    => '0600',
-    content => template('ssh_client/config.erb'),
-    require => File['/home/ubuntu/.ssh'],
-  }
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
 
-# Template for SSH client configuration
-file { '/etc/puppetlabs/code/modules/ssh_client/templates/config.erb':
-  ensure  => 'file',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "
-Host *
-    PubkeyAuthentication yes
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-",
+file_line { 'Delare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
