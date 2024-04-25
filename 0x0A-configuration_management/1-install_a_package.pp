b233a1c04c6c7aca615fff5b8af716e894ba0c3b
endywezy <endywezy@gmail.com>
2024-04-24 21:24:16 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
-# 1-install_a_package.pp
-
 package { 'python3-pip':
   ensure => installed,
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  command => '/usr/bin/pip3 install flask',  # Install latest version
+  unless => '/usr/bin/pip3 show flask',       # Check for any Flask installation
   require => Package['python3-pip'],
+  onFailure => { loglevel => 'err', message => 'Failed to install Flask' },
 }
+
