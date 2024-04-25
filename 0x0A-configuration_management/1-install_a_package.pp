4bdf6029547b1c2ec4e97b792db91bd9aeabc2de
endywezy <endywezy@gmail.com>
2024-04-25 01:22:08 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # installs package puppet-lint
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Install Flask package
+package { 'python3-flask': }  # Replace with the appropriate package name 
+
+# Check if Flask 2.1.0 is installed (Optional)
+exec { 'check_flask_version':
+  command => '/path/to/flask --version | grep 2.1.0',  # Replace with actual path if needed
+  unless => '/path/to/flask --version | grep 2.1.0',  # Check for expected version
 }
