4711f8ec672f6f82a45664506968532b52e4663b
endywezy <endywezy@gmail.com>
2024-04-29 20:16:22 -0700

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,12 @@
 #!/usr/bin/env bash
 # File: 2-puppet_custom_http_response_header.pp
 
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
-}
-
-# Define custom HTTP header response
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "server {
-                listen 80 default_server;
-                listen [::]:80 default_server;
-
-                server_name _;
-
-                location / {
-                    add_header X-Served-By $hostname;
-                    # Your other configuration directives here
-                    root /var/www/html;
-                    index index.html index.htm;
-                }
-            }",
-}
-
-# Ensure Nginx service is running
-service { 'nginx':
-  ensure => running,
+exec { 'configure_custom_http_header':
+  command  => 'apt-get -y update; \
+               apt-get -y install nginx; \
+               sed -i "/listen 80 default_server;/a add_header X-Served-By ${hostname};" /etc/nginx/sites-available/default; \
+               service nginx restart',
+  provider => shell,
+  path     => '/usr/bin:/bin', # Set proper PATH to avoid potential issues
+  require  => Package['nginx'], # Ensure Nginx is installed before executing this command
 }
