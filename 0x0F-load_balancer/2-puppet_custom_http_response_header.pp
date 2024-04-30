a0edca40bd47fb754f45f0af34a7091808cf2c64
endywezy <endywezy@gmail.com>
2024-04-29 21:20:19 -0700

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,10 @@
 #!/usr/bin/env bash
 # Puppet manifest to add a custom HTTP header
 
-# Define a class for Nginx configuration with custom header
-class custom_http_response_header {
-
-  # Ensure the nginx package is installed
-  package { 'nginx': ensure => present }
-
-  # Define a server block named 'default' with custom header
-  nginx::server {
-    name => 'default'
-    listen => [80]
-
-    # Configure the location block
-    location / {
-      # Add the custom header using add_header
-      add_header => { 'X-Served-By' => '$hostname' }
-
-      # Optional: Configure proxy pass if needed
-      # proxy_pass http://localhost:8080;
-    }
-  }
-
-  # Ensure the service is restarted after configuration changes
-  service { 'nginx': ensure => running, restart => true }
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
-
-# Include the custom_http_response_header class on the managed node
-class { 'custom_http_response_header': }
-
