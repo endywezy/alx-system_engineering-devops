6c8e08e8c000aa2dc98fb0dae283f4ab1f8ee8f8
endywezy <endywezy@gmail.com>
2024-04-29 21:30:17 -0700

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Puppet manifest to add a custom HTTP header
 
-exec { 'configure_nginx':
-  command     => 'apt-get -y update && apt-get -y install nginx && sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default && service nginx restart',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
+exec { 'command':
+command  => 'apt-get -y update;
+	apt-get -y install nginx;
+	sed -i "/listen 80 default_server;/a add_header X-Served-By ${::hostname};" /etc/nginx/sites-available/default;
+	service nginx restart',
+  provider => shell,
+  path     => '/usr/bin:/bin', # Set proper PATH to avoid potential issues
+  require  => Package['nginx'], # Ensure Nginx is installed before executing this command
 }
-
