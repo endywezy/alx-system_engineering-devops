619deb463ed236496c60d8e9d29dd08796243c23
endywezy <endywezy@gmail.com>
2024-05-20 15:37:22 -0700

    0x15 API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,44 +1,49 @@
 #!/usr/bin/python3
+"""
+Fetches to-do lists users from a REST API and saves them to a JSON file.
+"""
 
 import json
 import requests
 
+# API endpoints
+API_TODOS = 'https://jsonplaceholder.typicode.com/todos'
+API_USERS = 'https://jsonplaceholder.typicode.com/users'
+
 
 def fetch_data(url):
+    """
+    Fetches data from the given URL.
+    """
     response = requests.get(url)
     return response.json()
 
 
-def fetch_users_tasks():
-    users = fetch_data('https://jsonplaceholder.typicode.com/users')
-    todos = fetch_data('https://jsonplaceholder.typicode.com/todos')
+def main():
+    """
+    Main function to fetch data and export to JSON.
+    """
+    users = fetch_data(API_USERS)
+    todos = fetch_data(API_TODOS)
 
-    all_tasks = {}
-
+    user_dict = {}
     for user in users:
         user_id = user['id']
-        username = user['username']
-        user_tasks = []
+        user_dict[user_id] = []
 
-        for task in todos:
-            if task['userId'] == user_id:
-                task_info = {
-                    "username": username,
-                    "task": task['title'],
-                    "completed": task['completed']
-                }
-                user_tasks.append(task_info)
+    for todo in todos:
+        user_id = todo['userId']
+        task_dict = {
+            "username": next(user['username'] for user in users
+                             if user['id'] == user_id),
+            "task": todo['title'],
+            "completed": todo['completed']
+        }
+        user_dict[user_id].append(task_dict)
 
-        all_tasks[user_id] = user_tasks
-
-    return all_tasks
-
-
-def save_to_json(data, filename):
-    with open(filename, 'w') as file:
-        json.dump(data, file)
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(user_dict, json_file)
 
 
 if __name__ == "__main__":
-    data = fetch_users_tasks()
-    save_to_json(data, 'todo_all_employees.json')
+    main()
