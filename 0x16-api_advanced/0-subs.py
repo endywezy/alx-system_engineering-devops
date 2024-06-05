ade2e15f23e8f41c4ad262f7eb3da829a0eb2f58
endywezy <endywezy@gmail.com>
2024-06-05 05:33:58 +0100

    0x15 API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 """
 Function that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
+the number of subscribers for a given subreddit
 """
 import requests
-import sys
-
 
 def number_of_subscribers(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
+    """Returns the number of subscribers for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
 
-    headers = {
-        'User-Agent': u_agent
-    }
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
-        return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
+if __name__ == "__main__":
+    subreddit = input("Enter the subreddit name: ")
+    print(number_of_subscribers(subreddit))
