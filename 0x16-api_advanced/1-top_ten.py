3ddda5f3a2359861692ba8016e6cb1af10278896
endywezy <endywezy@gmail.com>
2024-06-05 05:46:29 +0100

    0x15 API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""
+    This module contains the function top_ten
+"""
+
 import requests
 from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
+    """
+        Returns the top ten posts for a given subreddit
+    """
     user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+    response = requests.get(url, headers=user)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
     try:
-        for post in url.get('data').get('children'):
+        for post in data.get('data').get('children'):
             print(post.get('data').get('title'))
     except Exception:
         print(None)
 
 
 if __name__ == "__main__":
-    top_ten(argv[1])
+    if len(argv) < 2:
+        print("Usage: {} <subreddit>".format(argv[0]))
+    else:
+        top_ten(argv[1])
