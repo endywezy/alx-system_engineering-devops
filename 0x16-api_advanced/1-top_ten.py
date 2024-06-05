61e212385c91590f3f2a2eb1c020409728459b84
endywezy <endywezy@gmail.com>
2024-06-05 05:51:28 +0100

    0x15 API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
-'''
-  this module contains the function top_ten
-'''
+"""
+    This module contains the function top_ten
+"""
+
 import requests
 from sys import argv
 
 
 def top_ten(subreddit):
-  '''
-    returns the top ten posts for a given subreddit
-  '''
-  user = {'User-Agent': 'Lizzie'}
-  url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                     .format(subreddit), headers=user).json()
+    """
+        Returns the top ten posts for a given subreddit
+    """
+    user = {'User-Agent': 'Lizzie'}
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+    response = requests.get(url, headers=user)
 
-  # Check if 'data' and 'children' exist before accessing them
-  if url.get('data') is not None and url['data'].get('children') is not None:
-    for post in url['data']['children']:
-      print(post['data'].get('title'))
-  else:
-    print("Error: Unable to retrieve data from Reddit")
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+    try:
+        for post in data.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
 
 if __name__ == "__main__":
-  # Check if there's a subreddit argument provided
-  if len(argv) < 2:
-    print("Usage: ./1-top_ten.py <subreddit>")
-    exit(1)
-
-  top_ten(argv[1])
+    if len(argv) < 2:
+        print("Usage: {} <subreddit>".format(argv[0]))
+    else:
+        top_ten(argv[1])
