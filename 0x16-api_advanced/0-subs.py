cbe43b5ce74c44a27c959e8a2a8ba9db5aa00935
endywezy <endywezy@gmail.com>
2024-06-06 15:54:53 -0700

    0x15 API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,18 +8,14 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """Queries to Reddit API"""
-    u_agent = "Mozilla/5.0"
-
-    headers = {"User-Agent": u_agent}
-
+    """ Queries to Reddit API """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
+    res = requests.get(url, allow_redirects=False)
     if res.status_code != 200:
         return 0
     dic = res.json()
-    if "data" not in dic:
+    if 'data' not in dic:
         return 0
-    if "subscribers" not in dic.get("data"):
+    if 'subscribers' not in dic.get('data'):
         return 0
-    return res.json()["data"]["subscribers"]
+    return dic['data']['subscribers']
