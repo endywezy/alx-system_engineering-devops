82b8a14107a46a3a9b4c88f3b136e7af315128d7
endywezy <endywezy@gmail.com>
2024-06-06 16:19:47 -0700

    0x15 API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,36 +1,58 @@
 #!/usr/bin/python3
+
 """
 Function that queries the Reddit API and prints
 the top ten hot posts of a subreddit
 """
+
 import requests
 import sys
 
 
 def top_ten(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Chrome/106.0'
+    """
+    Queries the Reddit API for the top ten hot posts of a subreddit
+    Args:
+        subreddit (str): The name of the subreddit to query
+
+    Returns:
+        None
+    """
+    user_agent = 'Mozilla/5.0'  # Updated user agent
 
     headers = {
-        'User-Agent': u_agent
+        'User-Agent': user_agent
     }
 
     params = {
         'limit': 10
     }
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
-    if res.status_code != 200:
-        print(None)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(f"Error: Could not retrieve data. Status code: {response.status_code}")
         return
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    if len(hot_posts) is 0:
-        print(None)
-    else:
-        for post in hot_posts:
-            print(post['data']['title'])
+
+    try:
+        data = response.json().get('data', {}).get('children', [])
+    except ValueError:
+        print("Error: Invalid JSON format in response")
+        return
+
+    if not data:
+        print(f"No hot posts found in subreddit: {subreddit}")
+        return
+
+    for post in data:
+        print(post['data']['title'])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: ./10-top_ten.py <subreddit>")
+        sys.exit(1)
+
+    subreddit = sys.argv[1]
+    top_ten(subreddit)
